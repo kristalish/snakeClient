67b2b35e5cb0df7056489038116f7c450e28d1d4
kristalish <krva14ab@student.cbs.dk>
2015-11-19 15:41:10 +0100

    implementeret login funktion DER VIRKER

--- js/Client.js
+++ js/Client.js
@@ -44,12 +44,17 @@ jQuery form for login via username or cbs mail
 
 $(".submit-button").click(function(){
 
-    debugger;
+    var Username = $('#username').val();
+    var Password = $('#password').val();
+    var user = {
+        username: Username,
+        password: Password
+    };
 
     $.ajax({
         type: 'POST',
-        url: 'http://localhost:9998/api/login/',
-        data: { username: $('#username').val(), password: $('#password').val() },
+        url: 'http://localhost:8888/api/login',
+        data: JSON.stringify(user),
         beforeSend:function(){
             // this is where we append a loading image
             $('form').append('loader');
@@ -57,7 +62,7 @@ $(".submit-button").click(function(){
         success:function(data){
             // successful request; do something with the data
             alert('Du er nu logged ind');
-            window.location.href = 'http://google.dk';
+            window.location.href = '../html/home.html';
         },
         error:function(){
             // failed request; give feedback to user
@@ -70,14 +75,13 @@ $(".submit-button").click(function(){
 
     });
 });
-
+/*
 $(".sign-up-button").click(function(){
 
-    debugger;
 
     $.ajax({
         type: 'POST',
-        url: 'http://localhost:9998/api/login/',
+        url: 'http://localhost:8888/api/login/',
         data: { username: $('#username').val(), password: $('#password').val() },
         beforeSend:function(){
             // this is where we append a loading image
@@ -98,7 +102,7 @@ $(".sign-up-button").click(function(){
 
 
 
-});
+});*/
 
 /*
 $.ajax({
@@ -122,3 +126,56 @@ $.ajax({
     }
 });
 */
+
+
+/*
+skal være med i game klassen
+ */
+
+/*$(function() {
+    $("#container").fadeTo("slow",1);
+});
+$.get("http://localhost:8888/api/users/", function( data )  {
+    //$( ".result" ).html( data );
+    for (var i=0;i<data.length;++i)
+    {
+        $('#frmOpponent')
+            .append($('<option>', { value : data[i].id })
+                .text(data[i].firstName + " " + data[i].lastName + " (" + data[i].userName + ")"));
+    }
+});*/
+
+
+/*function ConvertFormToJSON(form){
+    var array = jQuery("form").serializeArray();
+    var json = {};
+    jQuery.each(array, function() {
+        json[this.name] = this.value || '';
+    });
+    console.log(json);
+    return json;
+}
+// this is the id of the form
+$("#idForm").submit(function(e) {
+    var GameName = $('#frmGameName').val();
+    var Opponent = $('#frmOpponent').val();
+    var Host = $('#frmHost').val();
+    var HostControls = host_movements;
+    var url = "http://localhost:9998/api/game/"; // the script where you handle the form input.
+    var data = '{"gameName": "' + GameName + '", "opponent": '+ Opponent +', "host": '+ Host +', "hostControls": "' + HostControls + '"}';
+    $.ajax({
+        type: "POST",
+        url: url,
+        dataType: "JSON",
+        data: data, // serializes the form's elements.
+        success: function(response)
+        {
+            console.log(response.message); // show response
+            $("#overlay").fadeTo("slow",0);
+            $(".sk-folding-cube").delay(100).fadeTo(1600, 1);
+            setTimeout(function(){ document.location = '/';}, 4000 );
+        }
+    });
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+});*/
+

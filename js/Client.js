1f069c55b5bb6aa0a240da6b8c82ffd64c7c8560
kristalish <krva14ab@student.cbs.dk>
2015-11-23 13:39:37 +0100

    implementeret login funktion DER VIRKER. knyttet links til alle knapper.

--- js/Client.js
+++ js/Client.js
@@ -4,40 +4,6 @@
 
 $(document).ready(function () {
 
-/* dette er en test fra janik
-    console.log("TEST");
-// this is the id of the form
-    $("#login-block").on('Submit')(function(e) {
-        var Username = $('#userfrm').val();
-        var Password = $('#passfrm').val();
-        var url = "http://localhost:9998/api/login/"; // the script where you handle the form input.
-        var data = '{"username": "' + Username + '", "password": "'+ Password +'"}';
-        console.log(data);
-        $.ajax({
-            type: "POST",
-            url: url,
-            dataType: "JSON",
-            data: data, // serializes the form's elements.
-            error: function(response) {
-                $(".info").text("Unfortunately the server is not responding").fadeTo(1000, 1).fadeTo(2000, 0);
-            },
-            success: function(response)
-            {
-                console.log(response.message); // show response
-                if(response.userid) {
-                    $("#overlay").fadeTo("slow",0);
-                    $(".sk-folding-cube").delay(100).fadeTo(1600, 1);
-                }
-                else {
-                    $(".info").text("Wrong username or password. Please try again.").fadeTo(1000, 1).fadeTo(2000, 0);
-                    //document.location = '/login.php?info=Wrong username or password. Please try again.';
-                }
-            }
-        });
-        e.preventDefault(); // avoid to execute the actual submit of the form.
-    });
-*/
-
 /*
 jQuery form for login via username or cbs mail
 */
@@ -74,9 +40,14 @@ $(".submit-button").click(function(){
 
 
     });
+
+
+
 });
-/*
-$(".sign-up-button").click(function(){
+
+
+
+/*$(".sign-up-button").click(function(){
 
 
     $.ajax({
@@ -90,7 +61,7 @@ $(".sign-up-button").click(function(){
         success:function(data){
             // successful request; do something with the data
             alert('Du er nu logged ind');
-            window.location.href = 'http://google.dk';
+            window.location.href = '../html/signUp.html';
         },
         error:function(){
             // failed request; give feedback to user
@@ -102,80 +73,5 @@ $(".sign-up-button").click(function(){
 
 
 
-});*/
-
-/*
-$.ajax({
-    type: 'GET',
-    url: 'http://jsonplaceholder.typicode.com/posts',
-    beforeSend: function(){
-        $('#myFancyList').append('<li>Loader...</li>');
-    },
-    success:function(data){
-        // successful request; do something with the data
-        $('#myFancyList').html('');
-        for(var i = 0; i < data.length; i++){
-            $('#myFancyList').append('<li>' + data[i].title + '</li>');
-        }
-    },
-    error:function(){
-        // failed request; give feedback to user
-        $('#myFancyList').html('');
-        $('#myFancyList').append('<li>Det gik galt :/</li>');
-
-    }
-});
-*/
-
-
-/*
-skal være med i game klassen
- */
-
-/*$(function() {
-    $("#container").fadeTo("slow",1);
-});
-$.get("http://localhost:8888/api/users/", function( data )  {
-    //$( ".result" ).html( data );
-    for (var i=0;i<data.length;++i)
-    {
-        $('#frmOpponent')
-            .append($('<option>', { value : data[i].id })
-                .text(data[i].firstName + " " + data[i].lastName + " (" + data[i].userName + ")"));
-    }
-});*/
-
-
-/*function ConvertFormToJSON(form){
-    var array = jQuery("form").serializeArray();
-    var json = {};
-    jQuery.each(array, function() {
-        json[this.name] = this.value || '';
-    });
-    console.log(json);
-    return json;
-}
-// this is the id of the form
-$("#idForm").submit(function(e) {
-    var GameName = $('#frmGameName').val();
-    var Opponent = $('#frmOpponent').val();
-    var Host = $('#frmHost').val();
-    var HostControls = host_movements;
-    var url = "http://localhost:9998/api/game/"; // the script where you handle the form input.
-    var data = '{"gameName": "' + GameName + '", "opponent": '+ Opponent +', "host": '+ Host +', "hostControls": "' + HostControls + '"}';
-    $.ajax({
-        type: "POST",
-        url: url,
-        dataType: "JSON",
-        data: data, // serializes the form's elements.
-        success: function(response)
-        {
-            console.log(response.message); // show response
-            $("#overlay").fadeTo("slow",0);
-            $(".sk-folding-cube").delay(100).fadeTo(1600, 1);
-            setTimeout(function(){ document.location = '/';}, 4000 );
-        }
-    });
-    e.preventDefault(); // avoid to execute the actual submit of the form.
 });*/
 

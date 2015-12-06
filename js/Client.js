8e76c8462cd0be943f2dbbfcbe07bafa58e54301
kristalish <krva14ab@student.cbs.dk>
2015-12-06 16:42:45 +0100

    1. lavet delete game 2. lavet join game 3. lavet start game
     alle fungerer.

--- js/Client.js
+++ js/Client.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
             },
             success: function (data) {
                 // successful request; do something with the data
-                $.session.set('userId', data.userid)
+                $.session.set('userId', data.userid);
 
                 window.location.href = '../html/home.html';
             },
@@ -93,13 +93,14 @@ $(document).ready(function () {
 
         var createGame = {
             name: $('#gameName').val(),
+            mapSize: $('#mapSize').val(),
             opponent: {
                 id: $('#opponent').val()
             },
             host: {
                 id: $.session.get('userId'),
                 controls: $('#controls').val()
-            }
+            },
         };
 
 
@@ -154,6 +155,114 @@ $(document).ready(function () {
 
         }
     });
+});
+
+/*
+ Delete game
+ */
+$(".deleteGame").click(function () {
+
+    var gameid = $('#gameID').val(),
+        host = $.session.get('userId');
+
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8888/api/games/' + gameid,
+        data: JSON.stringify(gameid),
+        beforeSend: function () {
+            // this is where we append a loading image
+            $('form').append('loader');
+        },
+        success: function (data) {
+            // successful request; do something with the data
+            alert('Game deleted');
+            /*window.location.href = '../html/home.html';*/
+        },
+        error: function () {
+            // failed request; give feedback to user
+
+            alert('Der skete en fejl');
+
+        }
+    });
 
 
 });
+
+
+/*
+ Join game function
+ */
+$(".join-game").click(function () {
+
+    var joinGame = {
+        gameId: $('#gameID').val(),
+        opponent: {
+            id: $.session.get('userId')
+        }
+    };
+
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8888/api/games/join/',
+        data: JSON.stringify(joinGame),
+        beforeSend: function () {
+            // this is where we append a loading image
+            $('form').append('loader');
+        },
+        success: function (data) {
+            // successful request; do something with the data
+            alert('Congratulations, you have joined game');
+        },
+        error: function () {
+            // failed request; give feedback to user
+
+            alert('Der skete en fejl');
+
+        }
+    });
+
+
+});
+
+/*
+ Start game function
+ */
+$(".start-game").click(function () {
+
+    var startGame = {
+        gameId: $('#gameID').val(),
+        opponent: {
+          //  id: $.session.get('userId'),
+            controls: $('#controls').val()
+        }
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8888/api/games/start/',
+        data: JSON.stringify(startGame),
+        beforeSend: function () {
+            // this is where we append a loading image
+            $('form').append('loader');
+        },
+        success: function (data) {
+            // successful request; do something with the data
+            alert('Congratulations, you have started game');
+        },
+        error: function () {
+            // failed request; give feedback to user
+
+            alert('Der skete en fejl');
+
+        }
+    });
+
+
+});
+
+
+
+
